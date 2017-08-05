e6beebd29b745f8ebfcb61d0382dd0efeac542d0
Sato <lucasjks@gmail.com>
2017-08-05 20:41:56 -0300

    lyon and point operations

---
+++ lyon/plotter.py
@@ -0,0 +1,24 @@
+import plotly
+import plotly.plotly as py
+from plotly.graph_objs import *
+
+plotly.tools.set_credentials_file(username='satojk', api_key='nGrVGv2j7nWWcpCjLOMg')
+
+def movimentacoes():
+    trace0 = Scatter(
+        x=['2017-06-31', '2017-07-01', '2017-07-02', '2017-07-03', '2017-07-04', '2017-07-05', '2017-07-06'],
+        y=[10, 15, 13, 10, 20, 23, 12],
+        name="Emitidos"
+    )
+
+    trace1 = Scatter(
+        x=['2017-06-31', '2017-07-01', '2017-07-02', '2017-07-03', '2017-07-04', '2017-07-05', '2017-07-06'],
+        y=[16, 5, 11, 9, 10, 21, 20],
+        name="Trocados"
+    )
+
+    data = Data([trace0, trace1])
+    
+    plot_url = py.plot(data, filename = 'basic-line')
+
+movimentacoes()
