15a60784a5c8d073544041de498f82225a2bbc73
Sato <lucasjks@gmail.com>
2017-08-05 18:51:30 -0300

    populate svalbard

--- svalbard/populate.py
+++ svalbard/populate.py
@@ -11,3 +11,19 @@ printer.pprint(mock_data)
 conn = sqlite3.connect("svalbard.db")
 c = conn.cursor()
 
+for tablename in mock_data.keys():
+    for row in mock_data[tablename]:
+        insert_query = "INSERT INTO " + tablename + " "
+        fields = "("
+        values = "("
+        for field in row.keys():
+            fields += field + ", "
+            if type(row[field]) == str:
+                values += "'" + str(row[field]) + "'" + ", "
+            else:
+                values += str(row[field]) + ", "
+        insert_query += fields[:-2] + ") "
+        insert_query += "VALUES " + values[:-2] + ")"
+        print(insert_query)
+        c.execute(insert_query)
+conn.commit()
