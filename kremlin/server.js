9089d2f1c3aace1d4a3e11863ae9ff6c3e529c5b
csciutto <cristobal.sciutto@nubank.com>
2017-08-05 17:31:24 -0300

    kremlin takes america

--- kremlin/server.js
+++ kremlin/server.js
@@ -2,7 +2,7 @@ const express    = require('express');
 const bodyParser = require('body-parser');
 const sqlite3    = require('sqlite3').verbose();
 
-let db = new sqlite3.Database('../svalbard/svalbard.db' (err) => {
+let db = new sqlite3.Database('../svalbard/svalbard.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) console.error(err.message);
   console.log('Connected to in memory SQlite database.');
 });
